4ed1643382312a5a5e12be604615e5f7e0da52a1
AlekseyPortnov <portnovdna@gmail.com>
2022-04-12 23:22:08 +0300

    Добавлена функция умножить, удален текст

--- First/nums.py
+++ First/nums.py
@@ -1,11 +1,13 @@
 def plus(a,b):
     return a + b
 
-text = "new text"
-
 def minus(a,b):
     return a-b
+
+def mul(a,b):
+    return a*b
 
 if __name__== "__main__":
     print(plus(3,2))
     print(minus(7,3))
+    print(mul(3,3))
